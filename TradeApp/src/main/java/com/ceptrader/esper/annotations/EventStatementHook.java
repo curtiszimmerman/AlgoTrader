45e436829fd07bfed3273e301ff6129988d47790
sirinath1978m@gmail.com <sirinath1978m@gmail.com@78a5b570-e563-0aa7-2d2c-dff5a775fb16>
2011-04-10 18:14:08 +0000

    git-svn-id: http://algo-trader.googlecode.com/svn/trunk@91 78a5b570-e563-0aa7-2d2c-dff5a775fb16

--- TradeApp/src/main/java/com/ceptrader/esper/annotations/EventStatementHook.java
+++ TradeApp/src/main/java/com/ceptrader/esper/annotations/EventStatementHook.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface EplHook {
+public @interface EventStatementHook {
 	String statement();
 }
