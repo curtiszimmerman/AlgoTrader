fc6ff2cb4818071a77bdeb41cef92e33089b5f89
andyflury@gmail.com <andyflury@gmail.com@78a5b570-e563-0aa7-2d2c-dff5a775fb16>
2011-05-20 09:00:08 +0000

    not relevant to AlgoTrader
    git-svn-id: http://algo-trader.googlecode.com/svn/trunk@187 78a5b570-e563-0aa7-2d2c-dff5a775fb16

--- TradeApp/src/main/java/com/ceptrader/generic/esper/pojoevents/DataItem.java
+++
@@ -1,7 +0,0 @@
-package com.ceptrader.generic.esper.pojoevents;
-
-import java.io.Serializable;
-
-public interface DataItem extends Serializable {
-	
-}
--- TradeApp/src/main/java/com/ceptrader/util/Loggable.java
+++
@@ -1,5 +0,0 @@
-package com.ceptrader.util;
-
-public interface Loggable {
-	
-}
