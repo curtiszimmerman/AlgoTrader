482ccec48f399c9c7cdf49ed37f707cb9aeb6214
andyflury@gmail.com <andyflury@gmail.com@78a5b570-e563-0aa7-2d2c-dff5a775fb16>
2011-06-09 12:28:32 +0000

    revert changes on IbMarketDataServiceImpl and IbOrderServiceImpl removed IbServiceInterface from the model
    git-svn-id: http://algo-trader.googlecode.com/svn/trunk@225 78a5b570-e563-0aa7-2d2c-dff5a775fb16

--- AlgoTraderLight/code/src/main/java/com/algoTrader/service/ib/IbMarketDataServiceImpl.java
+++ AlgoTraderLight/code/src/main/java/com/algoTrader/service/ib/IbMarketDataServiceImpl.java
@@ -1,7 +1,5 @@
 package com.algoTrader.service.ib;
 
-import com.algoTrader.enumeration.ConnectionState;
-
 public class IbMarketDataServiceImpl extends IbMarketDataServiceBase {
 
 	@Override
@@ -12,29 +10,5 @@ public class IbMarketDataServiceImpl extends IbMarketDataServiceBase {
 	@Override
 	protected void handleRemoveFromExternalWatchlist() throws Exception {
 		// TODO Auto-generated method stub
-	}
-
-	@Override
-	protected void handleInit() throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void handleConnect() throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void handleDestroy() throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected ConnectionState handleGetConnectionState() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
--- AlgoTraderLight/code/src/main/java/com/algoTrader/service/ib/IbOrderServiceImpl.java
+++ AlgoTraderLight/code/src/main/java/com/algoTrader/service/ib/IbOrderServiceImpl.java
@@ -1,35 +1,10 @@
 package com.algoTrader.service.ib;
 
 import com.algoTrader.entity.trade.Order;
-import com.algoTrader.enumeration.ConnectionState;
 
 public class IbOrderServiceImpl extends IbOrderServiceBase {
 
 	protected void handleSendExternalOrder(String strategyName, Order order) throws Exception {
 		// TODO Auto-generated method stub
-	}
-
-	@Override
-	protected void handleInit() throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void handleConnect() throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void handleDestroy() throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected ConnectionState handleGetConnectionState() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
