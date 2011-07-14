7e185cf34d85af30e9cf6d92bb1687f47e2693b5
andyflury@gmail.com <andyflury@gmail.com@78a5b570-e563-0aa7-2d2c-dff5a775fb16>
2011-07-14 10:36:14 +0000

    add fxcm package and services
    git-svn-id: http://algo-trader.googlecode.com/svn/trunk@272 78a5b570-e563-0aa7-2d2c-dff5a775fb16

---
+++ AlgoTraderLight/code/src/main/java/com/algoTrader/service/fxcm/FXCMMarketDataServiceImpl.java
@@ -0,0 +1,14 @@
+package com.algoTrader.service.fxcm;
+
+import com.algoTrader.entity.security.Security;
+
+public class FXCMMarketDataServiceImpl extends FXCMMarketDataServiceBase {
+
+	protected void handlePutOnExternalWatchlist(Security security) throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+	protected void handleRemoveFromExternalWatchlist(Security security) throws Exception {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ AlgoTraderLight/code/src/main/java/com/algoTrader/service/fxcm/FXCMOrderServiceImpl.java
@@ -0,0 +1,10 @@
+package com.algoTrader.service.fxcm;
+
+import com.algoTrader.entity.trade.Order;
+
+public class FXCMOrderServiceImpl extends FXCMOrderServiceBase {
+
+	protected void handleSendExternalOrder(String strategyName, Order order) throws Exception {
+		// TODO Auto-generated method stub
+	}
+}
