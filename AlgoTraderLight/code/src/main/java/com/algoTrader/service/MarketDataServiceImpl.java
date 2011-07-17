4b53933792b7cc7380fea65a29bcd76c1397fdf3
andyflury@gmail.com <andyflury@gmail.com@78a5b570-e563-0aa7-2d2c-dff5a775fb16>
2011-07-17 20:21:01 +0000

    implement initWatchlist
    git-svn-id: http://algo-trader.googlecode.com/svn/trunk@283 78a5b570-e563-0aa7-2d2c-dff5a775fb16

--- AlgoTraderLight/code/src/main/java/com/algoTrader/service/MarketDataServiceImpl.java
+++ AlgoTraderLight/code/src/main/java/com/algoTrader/service/MarketDataServiceImpl.java
@@ -1,6 +1,7 @@
 package com.algoTrader.service;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -13,6 +14,7 @@ import com.algoTrader.entity.marketData.Bar;
 import com.algoTrader.entity.marketData.MarketDataEvent;
 import com.algoTrader.entity.marketData.Tick;
 import com.algoTrader.entity.security.Security;
+import com.algoTrader.util.ConfigurationUtil;
 import com.algoTrader.util.MyLogger;
 import com.algoTrader.vo.BarVO;
 import com.algoTrader.vo.RawTickVO;
@@ -20,6 +22,8 @@ import com.algoTrader.vo.RawTickVO;
 public abstract class MarketDataServiceImpl extends MarketDataServiceBase {
 
 	private static Logger logger = MyLogger.getLogger(MarketDataServiceImpl.class.getName());
+
+	private static boolean simulation = ConfigurationUtil.getBaseConfig().getBoolean("simulation");
 
 	protected Tick handleCompleteRawTick(RawTickVO rawTick) {
 
@@ -41,6 +45,17 @@ public abstract class MarketDataServiceImpl extends MarketDataServiceBase {
 		Collection<WatchListItem> watchListItems = marketDataEvent.getSecurity().getWatchListItems();
 		for (WatchListItem watchListItem : watchListItems) {
 			getRuleService().sendEvent(watchListItem.getStrategy().getName(), marketDataEvent);
+		}
+	}
+
+	protected void handleInitWatchlist() {
+	
+		if (!simulation) {
+
+			List<Security> securities = getSecurityDao().findSecuritiesOnWatchlist();
+			for (Security security : securities) {
+				putOnExternalWatchlist(security);
+			}
 		}
 	}
 
