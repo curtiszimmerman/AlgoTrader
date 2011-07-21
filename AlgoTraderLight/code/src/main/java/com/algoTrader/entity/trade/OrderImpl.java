c7ab0a7b41273e1fd1c91e9b08b5c2589815067b
andyflury@gmail.com <andyflury@gmail.com@78a5b570-e563-0aa7-2d2c-dff5a775fb16>
2011-07-21 14:27:43 +0000

    toString
    git-svn-id: http://algo-trader.googlecode.com/svn/trunk@300 78a5b570-e563-0aa7-2d2c-dff5a775fb16

--- AlgoTraderLight/code/src/main/java/com/algoTrader/entity/trade/OrderImpl.java
+++ AlgoTraderLight/code/src/main/java/com/algoTrader/entity/trade/OrderImpl.java
@@ -1,10 +1,13 @@
 package com.algoTrader.entity.trade;
 
+import org.apache.commons.lang.ClassUtils;
+
 public abstract class OrderImpl extends Order {
 
 	private static final long serialVersionUID = -6501807818853981164L;
 
 	public String toString() {
-		return String.valueOf(getNumber());
+
+		return getSide() + " " + getQuantity() + " " + ClassUtils.getShortClassName(this.getClass()) + " " + getSecurity().getSymbol();
 	}
 }
