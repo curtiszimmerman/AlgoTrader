248922a5258c13a42cf18c6596f7cf6e532cd136
andyflury@gmail.com <andyflury@gmail.com@78a5b570-e563-0aa7-2d2c-dff5a775fb16>
2011-08-29 14:51:54 +0000

    add modify order example
    PeriodicService:
    	createOrder -> createMarketOrder
    	add createLimitOrder
    	add modifyLimitOrder
    	add/update Subscribers
    	
    esper-periodic.cfg.xml:
    	add event-type MarketOrder and LimitOrder
    	
    module-periodic-main.epl:
    	split statements into groups: periodic trades / order cancellation / order modification / debug statements
    	add CREATE_LIMIT_ORDER & MODIFY_LIMIT_ORDER
    
    git-svn-id: http://algo-trader.googlecode.com/svn/trunk@329 78a5b570-e563-0aa7-2d2c-dff5a775fb16

--- AlgoTraderLightStrat/periodic/src/main/java/com/algoTrader/service/periodic/PeriodicServiceImpl.java
+++ AlgoTraderLightStrat/periodic/src/main/java/com/algoTrader/service/periodic/PeriodicServiceImpl.java
@@ -1,12 +1,14 @@
 package com.algoTrader.service.periodic;
 
+import java.math.BigDecimal;
+
 import org.apache.log4j.Logger;
 
 import com.algoTrader.ServiceLocator;
 import com.algoTrader.entity.Strategy;
 import com.algoTrader.entity.security.Security;
-import com.algoTrader.entity.trade.MarketOrderImpl;
-import com.algoTrader.entity.trade.Order;
+import com.algoTrader.entity.trade.LimitOrder;
+import com.algoTrader.entity.trade.MarketOrder;
 import com.algoTrader.enumeration.Side;
 import com.algoTrader.service.LookupService;
 import com.algoTrader.service.OrderService;
@@ -27,28 +29,72 @@ public class PeriodicServiceImpl {
 		this.orderService = orderService;
 	}
 
-	public void createOrder(int quantity) {
+	public void createMarketOrder(int quantity) {
 
 		Strategy strategy = this.lookupService.getStrategyByNameFetched("PERIODIC");
 		Security security = this.lookupService.getSecurityByIsin(underlayingIsin);
 
-		Order order = new MarketOrderImpl();
+		MarketOrder order = MarketOrder.Factory.newInstance();
 		order.setSecurity(security);
 		order.setStrategy(strategy);
 		order.setQuantity(Math.abs(quantity));
 		order.setSide(quantity > 0 ? Side.BUY : Side.SELL);
 
-		logger.info("placed order for quantity: " + quantity);
+		logger.info("placed market order for quantity: " + quantity);
 
 		this.orderService.sendOrder(order);
 	}
 
-	public static class CreateOrderSubscriber {
-	
+	public void createLimitOrder(int quantity, BigDecimal limit) {
+
+		Strategy strategy = this.lookupService.getStrategyByNameFetched("PERIODIC");
+		Security security = this.lookupService.getSecurityByIsin(underlayingIsin);
+
+		LimitOrder order = LimitOrder.Factory.newInstance();
+		order.setSecurity(security);
+		order.setStrategy(strategy);
+		order.setQuantity(Math.abs(quantity));
+		order.setSide(quantity > 0 ? Side.BUY : Side.SELL);
+		order.setLimit(limit.setScale(2, BigDecimal.ROUND_HALF_UP));
+
+		logger.info("placed order with limit: " + order.getLimit());
+
+		this.orderService.sendOrder(order);
+	}
+
+	public void modifyLimitOrder(LimitOrder order, BigDecimal limit) {
+
+		order.setLimit(limit.setScale(2, BigDecimal.ROUND_HALF_UP));
+
+		logger.info("set order limit to: " + order.getLimit());
+
+		this.orderService.modifyOrder(order.getNumber(), order);
+	}
+
+	public static class CreateMarketOrderSubscriber {
+
 		public void update(int quantity) {
+
+			PeriodicServiceImpl movService = (PeriodicServiceImpl) ServiceLocator.commonInstance().getService("periodicService");
+			movService.createMarketOrder(quantity);
+		}
+	}
+	
+	public static class CreateLimitOrderSubscriber {
+
+		public void update(int quantity, BigDecimal limit) {
 	
 			PeriodicServiceImpl movService = (PeriodicServiceImpl) ServiceLocator.commonInstance().getService("periodicService");
-			movService.createOrder(quantity);
+			movService.createLimitOrder(quantity, limit);
+		}
+	}
+
+	public static class ModifyLimitOrderSubscriber {
+
+		public void update(LimitOrder order, BigDecimal limit) {
+
+			PeriodicServiceImpl movService = (PeriodicServiceImpl) ServiceLocator.commonInstance().getService("periodicService");
+			movService.modifyLimitOrder(order, limit);
 		}
 	}
 }
